38359dc18fe1ffe111d8521da020197ba63f1f8d
Atkari Ariella <atkari.ariella@diak.szbi-pg.hu>
2024-10-09 12:19:59 +0200

    Add test_calculator with mult and div functions

--- test_calculator.py
+++ test_calculator.py
@@ -13,8 +13,20 @@ def test_sub():
     assert sub(-1, -2) == 1
     assert sub(0, 0) == 0
 
-# def test_mult():
-#     assert mult(1, 2) == 
-#     assert mult(-1, 2) == 
-#     assert mult(-1, -2) == 
-#     assert mult(0, 0) == 
+def test_mult():
+    assert mult(1, 2) == 2
+    assert mult(-1, 2) == -2
+    assert mult(-1, -3) == 3
+    assert mult(0, 2) == 0
+
+def test_div():
+    assert div(6, 2) == 3
+    assert div(-4, 2) == -2
+    assert div(-2, -2) == 1
+    assert div(0, 2) == 0
+    assert div(3, 0) == "Error: Division by zero"
+    
+
+# def test_div_zero():
+#     with pytest.raises(ZeroDivisionError):
+#         div(3, 0)
